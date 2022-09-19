8e5a1cf601700724a6d71c92738393e540c224eb
Danillo Postatni Moreno <dpostatni@gmail.com>
2022-09-19 23:41:12 +0100

    Adding Default Profile

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 /**
  * @author Danillo Postatni Moreno on 19/09/2022
  */
-@Profile("EN")
+@Profile({"EN", "default"})
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
     @Override
